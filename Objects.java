68ff38f6a4382019be36cc8fa621874abea06d5e
Edith Torres <tedith56@yahoo.com>
2024-12-21 11:25:05 -0700

    initial commit

---
+++ Objects.java
@@ -0,0 +1,22 @@
+package week03;
+
+public class Objects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 34;
+		
+		String name = "Sally Mae"; 
+		System.out.println(name.length());
+		System.out.println(name.charAt(6));
+		
+		Scanner s = new Scanner(System.in);
+		
+		int[] numbers = new int[3]; 
+		System.out.println(numbers.length);
+		
+		
+	}
+
+}
